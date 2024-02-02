9772cffefb01705e03e9e9642749ca81caab0ca0
pciuraj <pciuraj@sii.pl>
2024-02-02 10:59:35 +0100

    Refactor

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,20 @@
 #define ENABLE_GxEPD2_GFX 0
+
 #include <GxEPD2_BW.h>
 #include <GxEPD2_3C.h>
 #include <Fonts/FreeSans12pt7b.h>
 #include <TEF6686.h>
 #include <map>
+
 #define BACK_COLOR GxEPD_WHITE
 #define TEXT_COLOR GxEPD_BLACK
+#define FREQ_MAX 10800
+#define FREQ_MIN 8800
+#define FREQ_DISPLAY_MIN 8000
+#define FREQ_DISPLAY_MAX 11000
+#define QUALITY_MAX_VALUE 1200
+#define BUTTON_SEEK_UP 13
+#define BUTTON_SEEK_DOWN 12
 
 TEF6686 tef;
 uint8_t qualityOK;
@@ -22,7 +31,7 @@ GxEPD2_BW<GxEPD2_290_BS, GxEPD2_290_BS::HEIGHT> display(GxEPD2_290_BS(/*CS=5*/ 5
 void Seek(int step)
 {
   uint16_t f = tef.Currentfreq;
-  while (f < 10800)
+  while (f < FREQ_MAX)
   {
     f += step;
     if (qualityMap[f] > qualityThreshold)
@@ -45,14 +54,14 @@ void ScanAll(int step)
     tef.UpdateQualityStatus();
     qualityMap[tef.Currentfreq] = tef.quality;
     Serial.printf("Freq: %i, Quality: %i \n", tef.Currentfreq, tef.quality);
-  } while (tef.Currentfreq < 11000);
+  } while (tef.Currentfreq < FREQ_DISPLAY_MAX);
 }
 
 int16_t freqToX(uint16_t freq)
 {
   int length = (10 * (display.width() / 11) + 15) - (0 * (display.width() / 11) + 15);
   int start = 0 * (display.width() / 11) + 15;
-  float freqFact = (float)(freq - 8800) / (float)(10800 - 8800);
+  float freqFact = (float)(freq - FREQ_MIN) / (float)(FREQ_MAX - FREQ_MIN);
   return freqFact * length + start;
 }
 
@@ -74,14 +83,13 @@ static void UpdateScreen(void *parameter)
     {
       display.setTextSize(2);
       display.fillScreen(BACK_COLOR);
-      display.setTextColor(TEXT_COLOR); 
+      display.setTextColor(TEXT_COLOR);
       display.setCursor(0, 5);
       display.print(displayText);
       display.setFont(NULL);
       display.setCursor(0, 30);
       display.setTextSize(0);
       display.print(rtText);
-
 
       int vOffset = 10;
       display.drawLine(0, vOffset + display.height() / 2, display.width(), vOffset + display.height() / 2, TEXT_COLOR);
@@ -100,18 +108,18 @@ static void UpdateScreen(void *parameter)
       display.drawLine(freqToX(tef.Currentfreq), vOffset + display.height() / 2 - 20, freqToX(tef.Currentfreq), vOffset + display.height() / 2 + 20, TEXT_COLOR);
 
       int lastHeight = 0;
-      int lastX = freqToX(8000);
-      freq = 8000;
-      while (freq < 11000)
+      int lastX = freqToX(FREQ_DISPLAY_MIN);
+      freq = FREQ_DISPLAY_MIN;
+      while (freq < FREQ_DISPLAY_MAX)
       {
-        float qF = (float)qualityMap[freq] / (float)1200;
-        int barHeight = qF * 100 -vOffset;
+        float qF = (float)qualityMap[freq] / (float)QUALITY_MAX_VALUE;
+        int barHeight = qF * 100 - vOffset;
         display.drawLine(lastX, display.height() - lastHeight, freqToX(freq), display.height() - barHeight, TEXT_COLOR);
         lastX = freqToX(freq);
         lastHeight = barHeight;
         freq += 10;
       }
-      float qF = (float)qualityThreshold / (float)1200;
+      float qF = (float)qualityThreshold / (float)QUALITY_MAX_VALUE;
       int thresholdLineY = qF * 100 - vOffset;
       DrawHorizontalDottedLine(display.height() - thresholdLineY);
 
@@ -121,21 +129,19 @@ static void UpdateScreen(void *parameter)
 
 void setup()
 {
-  pinMode(12,INPUT_PULLUP);
-  pinMode(13,INPUT_PULLUP);
+  pinMode(BUTTON_SEEK_DOWN, INPUT_PULLUP);
+  pinMode(BUTTON_SEEK_UP, INPUT_PULLUP);
 
   Serial.begin(115200);
   tef.Init();
   tef.Audio_Set_Mute(0);
-  tef.Tune_To(tef.MODULE_FM, 8500);
-
   display.init(115200, true, 50, false);
   display.setPartialWindow(0, 0, display.width(), display.height());
   display.setRotation(1);
   display.clearScreen();
   xTaskCreate(UpdateScreen, "UpdateScreen", 20000, NULL, 5, NULL);
   ScanAll(10);
-  tef.Tune_To(tef.MODULE_FM, 8400);
+  tef.Tune_To(tef.MODULE_FM, FREQ_MIN);
   Seek(10);
 }
 
@@ -148,17 +154,13 @@ void loop()
     _lastRDSTime = currentTime;
     tef.UpdateRDSStatus();
     tef.UpdateQualityStatus();
-    sprintf(text, "Freq: %i.%i, MS: %i, TA: %i, PTY: %s, PS: %s,  RT: %s, Quality: %i", tef.Currentfreq / 100, tef.Currentfreq % 100, tef.ms, tef.ta, ptyLabels[tef.pty], tef.psText, tef.rtText, tef.quality);
-
-    Serial.println(text);
+    Serial.printf(text, "Freq: %i.%i, MS: %i, TA: %i, PTY: %s, PS: %s,  RT: %s, Quality: %i", tef.Currentfreq / 100, tef.Currentfreq % 100, tef.ms, tef.ta, ptyLabels[tef.pty], tef.psText, tef.rtText, tef.quality); 
   }
   sprintf(displayText, "FM %i.%i Mhz  %s", tef.Currentfreq / 100, tef.Currentfreq % 100, tef.psText);
   sprintf(rtText, "%s", tef.rtText);
   if (Serial.available())
   {
     int readed = Serial.read();
-    Serial.println("Readed: ");
-    Serial.println(readed);
     if (readed == 49)
       Seek(-10);
     else if (readed == 50)
@@ -182,12 +184,12 @@ void loop()
   }
   delay(10);
 
-  if(digitalRead(12)==0)
+  if (digitalRead(BUTTON_SEEK_DOWN) == 0)
   {
     Seek(-10);
     delay(300);
   }
-  if(digitalRead(13)==0)
+  if (digitalRead(BUTTON_SEEK_UP) == 0)
   {
     Seek(10);
     delay(300);
